c0fd8332767e0caa0af3d88967093d290fe182ff
Emanuelpa <epatinov@eafit.edu.co>
2025-05-07 09:45:55 -0500

    rds connection

--- BookStore-monolith/config.py
+++ BookStore-monolith/config.py
@@ -1,5 +1,15 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bookstore_user:bookstore_pass@db/bookstore'
+DB_USER = os.getenv("DB_USER", "bookstore_user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "bookstore_pass")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "bookstoredb")
+
+SQLALCHEMY_DATABASE_URI = (
+    f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
+
 SECRET_KEY = 'secretkey'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
